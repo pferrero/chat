2b35d100e9a4489b486ba90d087e430db2d6e1f9
Pablo Ferrero <ferreronicolas@hotmail.com>
2021-06-22 15:50:55 -0300

    Flask shell support

--- main.py
+++ main.py
@@ -1,1 +1,6 @@
-from chatapp import app
+from chatapp import app, db
+from chatapp.models import User, Message
+
+@app.shell_context_processor
+def make_shell_context():
+    return {"db": db, "User": User, "Message": Message}
