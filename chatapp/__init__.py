210af73536f14a862f6ff4a6ba3cf6534bc311b7
Pablo Ferrero <ferreronicolas@hotmail.com>
2021-06-28 12:36:14 -0300

    Add file logging file and handler.

--- chatapp/__init__.py
+++ chatapp/__init__.py
@@ -4,7 +4,8 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_migrate    import Migrate
 from flask_login import LoginManager
 import logging
-from logging.handlers import SMTPHandler
+from logging.handlers import SMTPHandler, RotatingFileHandler
+import os
 
 # app
 app = Flask(__name__)
@@ -20,6 +21,7 @@ print(app.config["MAIL_SERVER"])
 print(app.config["MAIL_PORT"])
 # logging
 if not app.debug:
+    # Configuration of the mail server to send errors by email
     if app.config["MAIL_SERVER"]:
         auth = None
         if app.config["MAIL_USERNAME"] or app.config["MAIL_PASSWORD"]:
@@ -37,6 +39,17 @@ if not app.debug:
         )
         mail_handler.setLevel(logging.ERROR)
         app.logger.addHandler(mail_handler)
+    # Configuration of the logs file
+    if not os.path.exists("logs"):
+        os.mkdir("logs")
+    file_handler = RotatingFileHandler("logs/chatapp.log", maxBytes=10240, backupCount=10)
+    file_handler.setFormatter(logging.Formatter(
+        "%(asctime)s %(levelname)s: %(message)s [in %(pathname)s:%(lineno)d]"))
+    file_handler.setLevel(logging.INFO)
+    app.logger.addHandler(file_handler)
+
+    app.logger.setLevel(logging.INFO)
+    app.logger.info("ChatApp startup")
 
 
 from chatapp import routes, models, errors
