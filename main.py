21effecd5370272756a24f5ae6a28199ba72e3ce
Pablo Ferrero <ferreronicolas@hotmail.com>
2021-07-05 23:13:03 -0300

    Add Open_Chat to main shell context.

--- main.py
+++ main.py
@@ -1,7 +1,12 @@
 from chatapp import app, db
-from chatapp.models import User, Message
+from chatapp.models import User, Message, Open_chat
 
 
 @app.shell_context_processor
 def make_shell_context():
-    return {"db": db, "User": User, "Message": Message}
+    return {
+        "db": db,
+        "User": User,
+        "Message": Message,
+        "Open_chat": Open_chat
+    }
