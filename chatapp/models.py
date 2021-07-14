786d9e6e8def48617298373eac2f3f15f8ed1200
Pablo Ferrero <ferreronicolas@hotmail.com>
2021-07-14 12:50:18 -0300

    Bug fix. The profile view now displays an image even if the user does not have an email yet.

--- chatapp/models.py
+++ chatapp/models.py
@@ -24,7 +24,8 @@ class User(UserMixin, db.Model):
         return check_password_hash(self.password_hash, password)
 
     def avatar_url(self, size):
-        digest = md5(self.email.lower().encode("utf-8")).hexdigest()
+        email = self.email or "default@default.com"
+        digest = md5(email.lower().encode("utf-8")).hexdigest()
         return "https://www.gravatar.com/avatar/{}?d=identicon&s={}".format(
             digest, size)
 
